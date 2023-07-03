5cc25826830976e66b05437397abfa4c22b8c157
yuluo <yuluo25@gmail.com>
2023-07-03 17:25:07 +0800

    On branch master

---
+++ src/main/java/link/yuluo/shorturl/model/UrlPO.java
@@ -0,0 +1,15 @@
+package link.yuluo.shorturl.model;
+
+import lombok.Data;
+
+/**
+ * @author yuluo
+ * @className: UrlPO
+ * @date 7/3/2023 17:21
+ * @description:
+ **/
+
+@Data
+public class UrlPO {
+    private
+}
