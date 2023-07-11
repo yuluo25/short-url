34e18c5d304a88e7f192442075603ae7f057cb8d
yuluo <yuluo25@gmail.com>
2023-07-11 22:22:51 +0800

    update

--- src/main/java/link/yuluo/shorturl/controller/ShortController.java
+++ src/main/java/link/yuluo/shorturl/controller/ShortController.java
@@ -5,6 +5,7 @@ import link.yuluo.shorturl.service.IUrlService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author yuluo
@@ -12,14 +13,16 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @date 7/10/2023 13:15
  * @description:
  **/
-@Controller
+@RestController
 public class ShortController {
 
     @GetMapping("/{originUrl}")
     public String getShortUrl(@PathVariable String originUrl){
         String shortUrl = urlService.getShortUrl(originUrl);
+        shortUrl="bilibili.com";
         return shortUrl;
     }
+
 
    public void visitOriginalWebsite(){
 
